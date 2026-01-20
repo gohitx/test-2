4214e84b40614164a810c28f27fb3ddb1a4f661e
gohitx <gohit21@gmail.com>
2026-01-20 16:30:20 -0500

    Install l

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,18 @@
+import { Rocket } from 'lucide-react-native';
 import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text className="text-2xl font-bold text-blue-400">New test APP 322</Text>
+    <View className="flex-1 items-center justify-center bg-white p-6">
+      <View className="mb-4 rounded-3xl bg-orange-50 p-4">
+        <Rocket size={48} color="purple" />
+      </View>
+      <Text className="text-3xl font-bold text-zinc-900 text-center">
+        New test APP HOME 1
+      </Text>
+      <Text className="text-base text-zinc-500 mt-2 text-center">
+        Welcome to your improved dashboard
+      </Text>
     </View>
   );
 }
