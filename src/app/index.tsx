075ebfa11ca8b681b712ec17bda5c110d1f43601
gohitx <gohit21@gmail.com>
2026-01-20 16:38:55 -0500

    🏗️ test one option

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white p-6">
       <View className="mb-4 rounded-3xl bg-orange-50 p-4">
-        <Rocket size={48} color="purple" />
+        <Rocket size={48} color="orange" />
       </View>
       <Text className="text-3xl font-bold text-zinc-900 text-center">
         New test APP HOME 1
